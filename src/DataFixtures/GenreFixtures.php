4f670cc262ad6821a84b71f34b460855f65f88cd
Ilyaçe Regaibi <ilyace.regaibi@gmail.com>
2018-10-25 11:57:57 +0200

    add tv serie genres

--- src/DataFixtures/GenreFixtures.php
+++ src/DataFixtures/GenreFixtures.php
@@ -12,13 +12,27 @@ class GenreFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $response = Request::get('https://api.themoviedb.org/3/genre/movie/list?api_key=3942737097dcd29145fe000304ac2294&language=fr-FR');
+        $film_genres = Request::get('https://api.themoviedb.org/3/genre/movie/list?api_key=3942737097dcd29145fe000304ac2294&language=fr-FR');
 
-        for ($i = 0; $i < count($response->body->genres); $i++) {
-            dump($response->body->genres[$i]->name);
-            $genreName = $response->body->genres[$i]->name;
+        $serie_genres = Request::get('https://api.themoviedb.org/3/genre/tv/list?api_key=3942737097dcd29145fe000304ac2294&language=fr-FR');
+
+        $all_genres = [];
+
+        for ($i = 0; $i < count($film_genres->body->genres); $i++) {
+            $genreName = $film_genres->body->genres[$i]->name;
+            array_push($all_genres, $genreName);
+        }
+
+        for ($i = 0; $i < count($serie_genres->body->genres); $i++) {
+            $genreName = $serie_genres->body->genres[$i]->name;
+            if(!in_array($genreName, $all_genres)) {
+                array_push($all_genres, $genreName);
+            }
+        }
+
+        for ($i = 0; $i < count($all_genres); $i++) {
             $genre = new Genre();
-            $genre->setName($genreName);
+            $genre->setName($all_genres[$i]);
             $manager->persist($genre);
         }
 
