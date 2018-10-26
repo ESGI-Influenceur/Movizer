9ce9d90abdd2803b4507db4ef1bb783cd8d5f9c0
Ilyaçe Regaibi <ilyace.regaibi@gmail.com>
2018-10-27 00:54:03 +0200

    update type on movie genre array collection

--- src/Entity/Movie.php
+++ src/Entity/Movie.php
@@ -76,7 +76,7 @@ class Movie
     /**
      * @ORM\ManyToMany(targetEntity="App\Entity\Genre", inversedBy="movies")
      */
-    private $genre;
+    private $genres;
 
     /**
      * @ORM\Column(type="integer")
@@ -85,7 +85,7 @@ class Movie
 
     public function __construct()
     {
-        $this->genre = new ArrayCollection();
+        $this->genres = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -228,15 +228,15 @@ class Movie
     /**
      * @return Collection|Genre[]
      */
-    public function getGenre(): Collection
+    public function getGenres(): Collection
     {
-        return $this->genre;
+        return $this->genres;
     }
 
     public function addGenre($genre): self
     {
-        if (!$this->genre->contains($genre)) {
-            $this->genre[] = $genre;
+        if (!$this->genres->contains($genre)) {
+            $this->genres[] = $genre;
         }
 
         return $this;
@@ -244,8 +244,8 @@ class Movie
 
     public function removeGenre(Genre $genre): self
     {
-        if ($this->genre->contains($genre)) {
-            $this->genre->removeElement($genre);
+        if ($this->genres->contains($genre)) {
+            $this->genres->removeElement($genre);
         }
 
         return $this;
