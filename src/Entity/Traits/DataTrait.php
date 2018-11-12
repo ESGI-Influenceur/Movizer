6536d8392bad6f79682958ce70599e4a2a0b00b0
Ilyaçe Regaibi <ilyace.regaibi@gmail.com>
2018-11-12 17:45:23 +0100

    use trait on movie and tv entities

---
+++ src/Entity/Traits/DataTrait.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity\Traits;
+use Doctrine\ORM\Mapping as ORM;
+
+trait DataTrait
+{
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $poster_path;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $backdrop_path;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $overview;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $average_note;
+
+    /**
+     * @return mixed
+     */
+    public function getPosterPath()
+    {
+        return $this->poster_path;
+    }
+
+    /**
+     * @param mixed $poster_path
+     */
+    public function setPosterPath($poster_path): void
+    {
+        $this->poster_path = $poster_path;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getBackdropPath()
+    {
+        return $this->backdrop_path;
+    }
+
+    /**
+     * @param mixed $backdrop_path
+     */
+    public function setBackdropPath($backdrop_path): void
+    {
+        $this->backdrop_path = $backdrop_path;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getOverview()
+    {
+        return $this->overview;
+    }
+
+    /**
+     * @param mixed $overview
+     */
+    public function setOverview($overview): void
+    {
+        $this->overview = $overview;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getAverageNote()
+    {
+        return $this->average_note;
+    }
+
+    /**
+     * @param mixed $average_note
+     */
+    public function setAverageNote($average_note): void
+    {
+        $this->average_note = $average_note;
+    }
+
+}
